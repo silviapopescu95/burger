413373f6db5023113b8faa7330a48d7aec452af6
Silvia Popescu <amazingaivlis@gmail.com>
2020-12-16 20:44:31 -0800

    Added functions to create, read, and update.

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,59 @@
 //import (require) connection.js into this file
+const { update } = require("../models/burger.js");
 var connection = require("./connection.js");
 
-// create selectAll() method
-// create insertOne() method
-// create updateOne() method
+var orm = {
+    // create selectAll() method
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+  
+    // create insertOne() method
+    create: function(table, cols, values, cb) {
+        queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(values.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+    },
+    // create updateOne() method
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+    },
+};
 
 // export  the ORM object
 module.exports = orm;
