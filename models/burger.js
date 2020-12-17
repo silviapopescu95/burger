c5174c696663a6e546e04ae57296d2f4ac239161
Silvia Popescu <amazingaivlis@gmail.com>
2020-12-16 20:42:41 -0800

    Added code to call the ORM functions.

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,26 @@
 // import orm.js here
-var orm = require("./orm.js");
+var orm = require("../config/orm.js");
 
 // create the code that will call the ORM functions using burger specific input for the ORM
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+};
 
 //export at the end of this file
 module.exports = burger;
