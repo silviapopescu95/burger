d6606e1d2bb7afc2729aaa2839a343a8951169d9
Silvia Popescu <amazingaivlis@gmail.com>
2020-12-17 23:58:45 -0800

    Added functions to handle SQL syntax.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,34 @@
 var connection = require("./connection.js");
 
+// helper function for SQL syntax
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+    return arr.toString();
+}
+
+// helper function to convert object to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations 
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  // translate array of strings to a single string
+  return arr.toString();
+}
+
 var orm = {
     all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -11,14 +40,14 @@ var orm = {
         });
     },
   
-    create: function(table, cols, values, cb) {
+    create: function(table, cols, vals, cb) {
         queryString = "INSERT INTO " + table;
 
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += printQuestionMarks(values.length);
+        queryString += printQuestionMarks(vals.length);
         queryString += ") ";
     
         console.log(queryString);
