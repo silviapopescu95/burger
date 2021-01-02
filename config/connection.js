f06ec138a2de290d84952d43078a8ce35e09eb31
Silvia Popescu <amazingaivlis@gmail.com>
2021-01-01 18:11:32 -0800

    Added jaws db to connection.js.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // set up code to connect node to mySQL
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Fo$teR<35",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Fo$teR<35",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
